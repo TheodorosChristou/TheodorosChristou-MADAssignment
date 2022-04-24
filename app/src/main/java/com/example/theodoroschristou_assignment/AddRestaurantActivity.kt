4f21b79c964358230a083fa9f331155fd0683561
TheodorosChristou <5chrit06@solent.ac.uk>
2022-04-24 18:55:01 +0100

    First-Second task complete

---
+++ app/src/main/java/com/example/theodoroschristou_assignment/AddRestaurantActivity.kt
@@ -0,0 +1,50 @@
+package com.example.theodoroschristou_assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+import android.view.View
+import android.widget.EditText
+import androidx.core.os.bundleOf
+
+class AddRestaurantActivity: AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_restaurant)
+
+        val addbtn = findViewById<Button>(R.id.btn1)
+        addbtn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val name = findViewById<EditText>(R.id.et1)
+        val address = findViewById<EditText>(R.id.et2)
+        val cuisine = findViewById<EditText>(R.id.et3)
+        val starr = findViewById<EditText>(R.id.et4)
+
+
+        var namex = ""
+        var addressx = ""
+        var cuisinex = ""
+        var starrx = 0.0
+
+        when (v?.id) {
+            R.id.btn1 -> {
+                namex = name.text.toString()
+                addressx = address.text.toString()
+                cuisinex = cuisine.text.toString()
+                starrx = starr.text.toString().toDouble()
+                addRestaurantLauncher(namex, addressx, cuisinex, starrx)
+            }
+        }
+    }
+
+    fun addRestaurantLauncher(name: String, address: String, cuisine: String, starr: Double ) {
+        val intent = Intent()
+        val bundle = bundleOf("com.example.name" to name, "com.example.address" to address, "com.example.cuisine" to cuisine, "com.example.starr" to starr, )
+        intent.putExtras(bundle)
+        setResult(RESULT_OK, intent)
+        finish()
+    }
+}
--- app/src/main/java/com/example/theodoroschristou_assignment/MainActivity.kt
+++ app/src/main/java/com/example/theodoroschristou_assignment/MainActivity.kt
@@ -1,11 +1,143 @@
 package com.example.theodoroschristou_assignment
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.osmdroid.config.Configuration
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.ItemizedIconOverlay
+import org.osmdroid.views.overlay.OverlayItem
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+class MainActivity : AppCompatActivity(), LocationListener {
+    var latitude = 0.0
+    var longitude = 0.0
+    lateinit var items: ItemizedIconOverlay<OverlayItem>
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        val markerGestureListener = object:ItemizedIconOverlay.OnItemGestureListener<OverlayItem>
+        {
+            override fun onItemLongPress(i: Int, item:OverlayItem ) : Boolean
+            {
+                Toast.makeText(this@MainActivity, item.snippet, Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onItemSingleTapUp(i: Int, item:OverlayItem): Boolean
+            {
+                Toast.makeText(this@MainActivity, item.snippet, Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+
+        val map1 = findViewById<MapView>(R.id.map1)
+        map1.controller.setZoom(16.0)
+        map1.controller.setCenter(GeoPoint(51.05, -0.72))
+        items = ItemizedIconOverlay(this, arrayListOf<OverlayItem>(), markerGestureListener)
+        map1.overlays.add(items)
+        requestLocation()
+    }
+
+    fun requestLocation() {
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            val mgr=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            mgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,0f,this)
+
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            0 -> {
+
+                if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    requestLocation()
+                } else {
+                    AlertDialog.Builder(this)
+                        .setPositiveButton("OK", null)
+                        .setMessage("This app will not work without the GPS permission enabled.")
+                        .show()
+                }
+            }
+
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu) : Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    val addRestaurantLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                it.data?.apply {
+                    val name = this.getStringExtra("com.example.name")
+                    val address = this.getStringExtra("com.example.address")
+                    val cuisine = this.getStringExtra("com.example.cusiine")
+                    val starr = this.getDoubleExtra("com.example.starr", 0.0)
+                    val newRestaurant = OverlayItem("${name}", "${name}",GeoPoint(latitude, longitude))
+                    items.addItem(newRestaurant)
+                }
+            }
+        }
+
+    override fun onOptionsItemSelected(item: MenuItem) : Boolean {
+        when(item.itemId) {
+            R.id.addrestaurant -> {
+                val intent = Intent(this,AddRestaurantActivity::class.java)
+                addRestaurantLauncher.launch(intent)
+                return true
+            }
+        }
+        return false
+    }
+
+
+    override fun onLocationChanged(newLoc: Location) {
+        val map1 = findViewById<MapView>(R.id.map1)
+        map1.controller.setZoom(16.0)
+        latitude = newLoc.latitude
+        longitude = newLoc.longitude
+        map1.controller.setCenter(GeoPoint(newLoc.latitude, newLoc.longitude))
+    }
+
+    override fun onProviderDisabled(provider: String) {
+        Toast.makeText (this, "Provider disabled", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onProviderEnabled(provider: String) {
+        Toast.makeText (this, "Provider enabled", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+
     }
 }
